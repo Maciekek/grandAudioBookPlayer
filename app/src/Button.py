ead07bcc15d81a6a31c54cc78c1f832b43e830d1
maciejkucharski <maciej.kucharski@senti1.com>
2020-05-01 22:02:07 +0200

    git button callback

--- app/src/Button.py
+++ app/src/Button.py
@@ -13,7 +13,7 @@ class Button:
         GPIO.setup(buttonGPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.add_event_detect(buttonGPIO, GPIO.RISING, callback=self.button_callback)  # Setup event on pin 10 rising edge
 
-    def button_callback(self):
+    def button_callback(self, channel):
         log('presed presedpresedpresedpresedpresedpresedpresed')
 
     def onPressed(self):
